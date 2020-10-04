e02cc106d4d928c894654f8dea4ed254a57439dd
Turaboy Holmirzaev <turabekh@gmail.com>
2020-10-03 20:55:26 -0400

    RepositoryBase added

---
+++ Interfaces/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Interfaces
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression);
+        void Create(T model);
+        void Update(T model);
+        void Delete(T model);
+
+    }
+}
---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Repository
+{
+        public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+        {
+            protected DataContext DataContext { get; set; }
+            public RepositoryBase(DataContext dataContext)
+            {
+                DataContext = dataContext;
+            }
+            public IQueryable<T> GetAll()
+            {
+                return DataContext.Set<T>().AsNoTracking();
+            }
+            public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression)
+            {
+                return DataContext.Set<T>().Where(expression).AsNoTracking();
+            }
+            public void Create(T entity)
+            {
+                DataContext.Set<T>().Add(entity);
+            }
+            public void Update(T entity)
+            {
+                DataContext.Set<T>().Update(entity);
+            }
+            public void Delete(T entity)
+            {
+                DataContext.Set<T>().Remove(entity);
+            }
+        }
+    }
